83f7ae42ba7baac71d4be61b122358827e74067b
kgall <karl.forrest.gall@ibm.com>
2019-08-12 15:19:54 -0400

    init repo.

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import environment from "./Environment.js";
+import { QueryRenderer, graphql } from "react-relay";
+
+class App extends React.Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query AppQuery {
+            OpenMics {
+              id
+              location
+              description
+              phone
+              author
+            }
+          }
+        `}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>{error.message}</div>;
+          } else if (props) {
+            return (
+            <div>
+              {props.OpenMics.map((mic, key) => 
+              <div>
+                    <span style={{display: 'block'}}>{mic.author}</span>
+                    <span style={{display: 'block'}} key={key}>{mic.location}</span>
+                    <span style={{display: 'block'}}>{mic.description}</span>
+                    <span style={{display: 'block'}}>{mic.phone}</span>
+                    <br/>
+              </div>
+                )}
+            </div>
+            )
+          }
+          return null;
+        }}
+      />
+    );
+  }
+}
+
+export default App;
---
+++ src/__generated__/AppQuery.graphql.js
@@ -0,0 +1,117 @@
+/**
+ * @flow
+ * @relayHash a86136874d4ee647cfdd2ead86b82af7
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest } from 'relay-runtime';
+export type AppQueryVariables = {||};
+export type AppQueryResponse = {|
+  +OpenMics: ?$ReadOnlyArray<?{|
+    +id: ?string,
+    +location: ?string,
+    +description: ?string,
+    +phone: ?string,
+    +author: ?string,
+  |}>
+|};
+export type AppQuery = {|
+  variables: AppQueryVariables,
+  response: AppQueryResponse,
+|};
+*/
+
+
+/*
+query AppQuery {
+  OpenMics {
+    id
+    location
+    description
+    phone
+    author
+  }
+}
+*/
+
+const node/*: ConcreteRequest*/ = (function(){
+var v0 = [
+  {
+    "kind": "LinkedField",
+    "alias": null,
+    "name": "OpenMics",
+    "storageKey": null,
+    "args": null,
+    "concreteType": "OpenMic",
+    "plural": true,
+    "selections": [
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "id",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "location",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "description",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "phone",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "author",
+        "args": null,
+        "storageKey": null
+      }
+    ]
+  }
+];
+return {
+  "kind": "Request",
+  "fragment": {
+    "kind": "Fragment",
+    "name": "AppQuery",
+    "type": "Query",
+    "metadata": null,
+    "argumentDefinitions": [],
+    "selections": (v0/*: any*/)
+  },
+  "operation": {
+    "kind": "Operation",
+    "name": "AppQuery",
+    "argumentDefinitions": [],
+    "selections": (v0/*: any*/)
+  },
+  "params": {
+    "operationKind": "query",
+    "name": "AppQuery",
+    "id": null,
+    "text": "query AppQuery {\n  OpenMics {\n    id\n    location\n    description\n    phone\n    author\n  }\n}\n",
+    "metadata": {}
+  }
+};
+})();
+// prettier-ignore
+(node/*: any*/).hash = '13931a199f445baac89946705a045d5a';
+module.exports = node;
